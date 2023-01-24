e972f806b39452630e764bb93b6103fa019da518
Curtis Johnson <curtiscarljohnson@gmail.com>
2023-01-24 14:24:46 -0700

    added pid stuff

--- ECEnRacer.py
+++ ECEnRacer.py
@@ -28,6 +28,7 @@ from RealSense import *
 import numpy as np
 import imutils
 import cv2
+from simple_pid import PID
 
 enableDepth = True
 rs = RealSense("/dev/video2", RS_VGA, enableDepth)		# RS_VGA, RS_720P, or RS_1080P
@@ -43,16 +44,116 @@ Car.pid(1)          # Use PID control
 (time, rgb, depth, accel, gyro) = rs.getData(False)
 cv2.namedWindow('RGB', cv2.WINDOW_NORMAL)
 
+################################ FUNCTIONS ################################
+
+def get_yellow_blob_x(bgr_img):
+    """gets x coord of centerline blob to follow
+
+    Args:
+        img (_type_): BGR image from realsense camera
+
+    Returns:
+        float: x coordinate of the closest blob
+    """
+    rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
+    # plt.imshow(rgb_img)
+    # plt.show()
+    # Get grayscale image with only centerline (yellow colors)
+    lower_yellow = np.array([200,200,0])
+    upper_yellow = np.array([255,255,0])
+    centerline_gray_img = cv2.inRange(rgb_img, lower_yellow, upper_yellow) # get only yellow colors in image
+
+    # Get Contours for center line blobs
+    contours, hierarchy = cv2.findContours(centerline_gray_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    centers = []
+    for i in contours:
+        M = cv2.moments(i)
+        if M['m00'] != 0:
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            if cy > rgb_img.shape[0]//2:
+                centers.append((cx, cy))
+
+    centers.sort(key = lambda x: x[1])
+
+    # Make sure that their is a yellow blob found
+    if len(centers) == 0:
+        return "None"
+    else:
+        return centers
+
+def draw_centers(img, centers):
+    # Draws given centers onto given image
+    if len(centers) > 1 and centers != "None":
+        print(f"centers;: {centers}")
+        for point in centers:
+            cv2.circle(img, point, 15, (0, 0, 255), -1) 
+            # args: img to draw on, point to draw, size of circle, color, line width (-1 defaults to fill)
+
+################################ MAIN LOOP ################################
+
+## SETUP PID Controller
+pid = PID()
+pid.Ki = -.01*0
+pid.Kd = -.01*0
+pid.Kp = -30/350 #degrees per pixel
+frameUpdate = 1
+pid.sample_time = frameUpdate/30.0
+pid.output_limits = (-30,30)
+desXCoord = rgb.shape[0]//3
+pid.setpoint = desXCoord
+
+i = 1
+angle = 0
+FAST_SPEED = .8
+SLOW_SPEED = 0.5
+speed = FAST_SPEED
+blob_lost = False
+draw_centers_bool = True
+centers = []
+
+Car.drive(speed)
+
 # You can use kd and kp commands to change KP and KD values.  Default values are good.
 # loop over frames from Realsense
-while True:
+while(True):
 	(time, rgb, depth, accel, gyro) = rs.getData(False)
+
+	# control loop
+	if i%frameUpdate == 0:
+		i = 0
+		centers = get_yellow_blob_x(rgb)
+		# if len(centers) > 4:
+		#     blobToFollowCoords = centers[-5]
+		# else:
+
+		if centers == "None" and not blob_lost:
+			blob_lost = True
+			speed = SLOW_SPEED
+			angle = -30.0 
+		elif centers != "None" or not blob_lost:
+			blobToFollowCoords = centers[-1]
+			blobX = blobToFollowCoords[0]
+
+			blob_lost = False
+			speed = FAST_SPEED
+			angle = pid(blobX)
+
+		Car.steer(angle)
+		Car.drive(speed)
+	i+=1
+
+	if draw_centers_bool:
+		draw_centers(rgb, centers)
 	
 	cv2.imshow("RGB", rgb)
 
 	if (cv2.waitKey(1) == ord('q')):
 		cv2.destroyAllWindows()
 		break
+
+
 #     cv2.imshow("Depth", depth)
 
 #     '''
