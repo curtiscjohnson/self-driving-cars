63d72397ab75b033b0a90b650238fcbb259a5ecf
Haley Priscilla Sanders <hpsanders98@gmail.com>
2023-02-28 12:27:02 -0700

    found good complicated map to train on

--- OriginalSimulatorExample.py
+++ OriginalSimulatorExample.py
@@ -48,10 +48,10 @@ cv2.createTrackbar("steer", "map", controlSteer, maxSteer * 2, updateSteer)
 
 # Can pass map parameters:
 mapParameters = {
-    "loops": 4,
-    "size": (10, 10),
+    "loops": 5,
+    "size": (7, 7),
     "expansions": 40,
-    "complications": 0
+    "complications": 3
 }
 
 # Can also pass car parameters for max/min speed, etc
@@ -64,13 +64,13 @@ carParameters = {
 }
 
 # sim.start(,carParameters=carParameters)
-seed = 1333
+seed = 10000
 random.seed(seed)
 # random seed for consistent maps
 # can also pass a start location if you know the code: (y tile index, x tile index, position index, direction index)
 # - position index is from 0-(number of connections the tile has - 1), so a straight is 0 or 1, a t is 0, 1, or 2.
 # - direction index is 0 or 1 for normal or reversed.
-sim.start(mapSeed=seed, mapParameters=mapParameters, carParameters=carParameters, startPoint=(0,1,0,1))
+sim.start(mapSeed=seed, mapParameters=mapParameters, carParameters=carParameters)
 
 car = sim.ackermann
 
@@ -85,7 +85,6 @@ while(True):
     cv2.imshow("map", newImg)
     cv2.imshow("car", img)
     distToCenter, bearingOffset = sim.getStats()
-    print(bearingOffset)
     if (cv2.waitKey(1) == ord('q')): # this simulator waits for a keypress every frame because otherwise it'd be really hard to control I think.
         # You could probably implement arrow key control, but... I didn't.  So.
         # Press q to quit, anything else to advance 1/30 second.
